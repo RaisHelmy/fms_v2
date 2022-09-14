c944abfe5f9175d86686cf0b76f5a1be222e5b1d
RaisHelmy <44133630+RaisHelmy@users.noreply.github.com>
2022-09-14 11:57:12 +0800

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,13 @@
+jsPanel.create({
+  panelSize: '450 500',
+  headerTitle: "IPD Information",
+   content: '<p style="padding-left: 10px;">IPD Sentul</p><p style="padding-left: 12px;">IPD Another Information </p><iframe src="https://raishelmy.github.io/timetable_fms/" title="shield_map_3d_Map" width="100%" height="720px"></iframe>',
+  position: {
+    //my:      "left-top",
+    //at:      "left-top",
+    //offsetX: 150,
+    //offsetY: 150,
+    maxLeft: 10,
+    maxTop: 50
+  }
+});
---
+++ scriptPaneld.js
@@ -0,0 +1,15 @@
+function kPaneld() { 
+  jsPanel.create({
+    panelSize: '450 500',
+    headerTitle: "IPD Information",
+     content: '<p style="padding-left: 10px;">IPD Sentul</p><p style="padding-left: 12px;">IPD Another Information </p><iframe src="https://raishelmy.github.io/jadual_fms/" title="shield_map_3d_Map" width="100%" height="720px"></iframe>',
+    position: {
+      //my:      "left-top",
+      //at:      "left-top",
+      //offsetX: 150,
+      //offsetY: 150,
+      maxLeft: 10,
+      maxTop: 120
+    }
+  });
+}
